3c6086d7b220b07750535c66b997d4071c634a45
nutty001 <90544250+nutty001@users.noreply.github.com>
2025-02-28 20:14:31 +0530

    first commit

---
+++ frontend/notes-app/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#2B85FF",
+        secondary:"#EF863E",
+      },
+    },
+  },
+  plugins: [],
+}
+
